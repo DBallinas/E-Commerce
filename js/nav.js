92f61edf3d38c6ff500adabbfb8e6f2c4de2eeca
josevazquezrmx <jose.vazquez2692@alumnos.udg.mx>
2022-06-24 19:57:20 -0500

    Se corrigio vista general de todos los productos, se corrigio barra de navegacion, en un solo js, enlazada a todas las paginas segun niveles

--- js/nav.js
+++ js/nav.js
@@ -6,7 +6,7 @@ class navcomponent extends HTMLElement{  //Se define el nombre del componente co
         this.innerHTML = 
       `<nav class="navbar navbar-expand-lg navbar-dark nav-menu" style="background-color: #50CCFF;">
         <div class="container-fluid">
-          <a class="navbar-brand" href="./index.html"><img src="./img/G-SPORTS-LOGO.png" width="200px" alt="logo-gsports"></a>
+          <a class="navbar-brand" href="../../index.html"><img src="../../img/G-SPORTS-LOGO.png" width="200px" alt="logo-gsports"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -14,10 +14,10 @@ class navcomponent extends HTMLElement{  //Se define el nombre del componente co
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mt-4 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./index.html">Inicio</a>
+                <a class="nav-link active" aria-current="page" href="../../index.html">Inicio</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white" href="./colecciones/lanzamientos.html">Lanzamientos</a>
+                <a class="nav-link text-white" href="../../colecciones/lanzamientos.html">Lanzamientos</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button"
@@ -25,19 +25,19 @@ class navcomponent extends HTMLElement{  //Se define el nombre del componente co
                   Categorías
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="./colecciones/calzado/calzado.html">Calzado</a></li>
-                  <li><a class="dropdown-item" href="./colecciones/ropa/ropa.html">Ropa</a></li>
-                  <li><a class="dropdown-item" href="./colecciones/aparatos/aparatos.html">Aparatos</a></li>
-                  <li><a class="dropdown-item" href="./colecciones/accesorios/accesorios.html">Accesorios</a></li>
-                  <li><a class="dropdown-item" href="./colecciones/suplementos/suplementos.html">Suplementos</a></li>
+                  <li><a class="dropdown-item" href="../../colecciones/calzado/calzado.html">Calzado</a></li>
+                  <li><a class="dropdown-item" href="../../colecciones/ropa/ropa.html">Ropa</a></li>
+                  <li><a class="dropdown-item" href="../../colecciones/aparatos/aparatos.html">Aparatos</a></li>
+                  <li><a class="dropdown-item" href="../../colecciones/accesorios/accesorios.html">Accesorios</a></li>
+                  <li><a class="dropdown-item" href="../../colecciones/suplementos/suplementos.html">Suplementos</a></li>
                   <li>
                     <hr class="dropdown-divider">
                   </li>
-                  <li><a class="dropdown-item" href="./colecciones/todoslosproductos1.html">Ver todo</a></li>
+                  <li><a class="dropdown-item" href="../../colecciones/todoslosproductos1.html">Ver todo</a></li>
                 </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white" href="./colecciones/rebajas.html">Rebajas</a>
+                <a class="nav-link text-white" href="../../colecciones/rebajas.html">Rebajas</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button"
@@ -45,9 +45,9 @@ class navcomponent extends HTMLElement{  //Se define el nombre del componente co
                   Nosotros
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="./about-us.html">Nosotros</a></li>
-                  <li><a class="dropdown-item" href="./contact.html">Contacto</a></li>
-                  <li><a class="dropdown-item" href="./preguntasfrecuentes.html">Preguntas frecuentes</a></li>
+                  <li><a class="dropdown-item" href="../../about-us.html">Nosotros</a></li>
+                  <li><a class="dropdown-item" href="../../contact.html">Contacto</a></li>
+                  <li><a class="dropdown-item" href="../../preguntasfrecuentes.html">Preguntas frecuentes</a></li>
                 </ul>
               </li>
             </ul>

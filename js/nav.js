1f47d5f1b3a4ae0e246f279639d7d62cfed43c6a
paco <pacojg.ti@gmail.com>
2022-07-05 21:21:27 -0500

    Se agregó un café

--- js/nav.js
+++ js/nav.js
@@ -12,7 +12,7 @@ class navcomponent extends HTMLElement{  //Se define el nombre del componente co
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mt-4 mb-lg-0">
+            <ul class="nav" class="navbar-nav me-auto mt-4 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="../../index.html">Inicio</a>
               </li>

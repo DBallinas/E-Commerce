a5847e7d84e013de9527c75e14096e3e0ba35c06
josevazquezrmx <jose.vazquez2692@alumnos.udg.mx>
2022-07-06 06:44:02 -0500

    Corregi diversos errores dentro de todo el documento

--- js/nav.js
+++ js/nav.js
@@ -14,10 +14,9 @@ class navcomponent extends HTMLElement{  //Se define el nombre del componente co
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="nav" class="navbar-nav me-auto mt-4 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="../../index.html">Inicio</a>
+                <a class="nav-link active text-white" aria-current="page" href="../../index.html">Inicio</a>
               </li>
               <li class="nav-item">
-<<<<<<< HEAD
                 <a class="nav-link text-white" href="../../categorias/lanzamientos.html">Lanzamientos</a>
               </li>
               <li class="nav-item">
@@ -25,30 +24,21 @@ class navcomponent extends HTMLElement{  //Se define el nombre del componente co
             </li>
               <li class="nav-item">
                 <a class="nav-link text-white" href="../../categorias/rebajas.html">Rebajas</a>
-=======
-                <a class="nav-link" href="../../colecciones/lanzamientos.html">Lanzamientos</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link href="../../colecciones/todoslosproductos">Productos</a>
-            </li>
-              <li class="nav-item">
-                <a class="nav-link" href="../../colecciones/rebajas.html">Rebajas</a>
->>>>>>> francisco
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button"
                   data-bs-toggle="dropdown" aria-expanded="false">
                   Nosotros
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="../../about-us.html">Nosotros</a></li>
-                  <li><a class="dropdown-item" href="../../contact.html">Contacto</a></li>
-                  <li><a class="dropdown-item" href="../../preguntasfrecuentes.html">Preguntas frecuentes</a></li>
-                  <li><a class="dropdown-item" href="../RegistroUsuarios/RegistroUsuarios.html">Regístrate</a></li>
+                  <li><a class="dropdown-item text-black" href="../../about-us.html">Nosotros</a></li>
+                  <li><a class="dropdown-item text-black" href="../../contact.html">Contacto</a></li>
+                  <li><a class="dropdown-item text-black" href="../../preguntasfrecuentes.html">Preguntas frecuentes</a></li>
+                  <li><a class="dropdown-item text-black" href="../RegistroUsuarios/RegistroUsuarios.html">Regístrate</a></li>
                   </ul>
               </li>
             </ul>
-            <form class="d-flex align-items-center mt-4 mb-2">
+            <form class="d-flex align-items-center mt-8 mb-2">
               <input class="form-control form-control-sm me-2 mt-1 mb-0" type="search" placeholder="Buscar productos" aria-label="Search">
               <a><i id="btn1" class="bi bi-search mt-5 mb-0 ms-1 text-white"></i></a>
               <a href="../iniciodesesion.html"><i id="btn2" class="bi bi-person mt-5 mb-0 ms-5 text-white h2"></i></a>
